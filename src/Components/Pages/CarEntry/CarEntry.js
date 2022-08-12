76166f6959e46c1dcedfe5ea134ec3b9dc1847ec
sakib-saron <sakibahcoder@gmail.com>
2022-08-12 17:53:42 +0600

    Add or remove done

--- src/Components/Pages/CarEntry/CarEntry.js
+++ src/Components/Pages/CarEntry/CarEntry.js
@@ -4,17 +4,65 @@ import { useParams } from "react-router-dom";
 const CarEntry = () => {
   const { id } = useParams();
   const [cars, setCars] = useState({});
+  const [carsQty, setCarsQty] = useState(0);
+  const [add, setAdd] = useState(0);
+
+  // console.log(typeof carsQty, carsQty, cars.qty);
 
   useEffect(() => {
     fetch(`http://localhost:5000/thecar/${id}`)
       .then((res) => res.json())
-      .then((data) => setCars(data));
-  }, []);
+      .then((data) => {
+        setCars(data);
+        setCarsQty(data.qty);
+      });
+  }, [id]);
+
+  // useEffect(() => {
+  //   let qtyCars = cars.qty;
+  //   setCarsQty(qtyCars);
+  // }, [cars.qty]);
+
+  const handleQty = () => {
+    setCarsQty(parseInt(carsQty) - 1);
+    cars.qty = parseInt(cars.qty) - 1;
+    const totalqty = cars.qty;
+
+    console.log(cars.qty);
+
+    // const totalQty = parseInt(carsQty);
+
+    fetch(`http://localhost:5000/quantity/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ totalqty }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    const totalqty = parseInt(carsQty) + parseInt(add);
+    setCarsQty(totalqty);
+
+    fetch(`http://localhost:5000/quantity/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ totalqty }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div>
       <h2 className="text-center font-bold text-2xl my-5">
-        Total {cars.name} Available in Stock : {cars.qty}
+        Total {cars.name} Available in Stock : {carsQty}
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4">
@@ -23,6 +71,7 @@ const CarEntry = () => {
             Click here to deliverd 1 Items from Stock
           </h2>
           <button
+            onClick={handleQty}
             type="button"
             className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-5"
           >
@@ -30,7 +79,7 @@ const CarEntry = () => {
           </button>
         </div>
         <div className="add border-2 rounded-xl border-[#0694a2] p-3">
-          <form>
+          <form onSubmit={handlesubmit}>
             <div className="relative z-0 mb-6 w-full group">
               <label
                 for="email"
@@ -39,6 +88,7 @@ const CarEntry = () => {
                 Add Items for new stock
               </label>
               <input
+                onBlur={(e) => setAdd(e.target.value)}
                 type="number"
                 id="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
