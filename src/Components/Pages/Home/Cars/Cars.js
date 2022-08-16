0315d86af767a4447c95963e209d2cb62373ef4f
sakib-saron <sakibahcoder@gmail.com>
2022-08-16 21:09:31 +0600

    animation

--- src/Components/Pages/Home/Cars/Cars.js
+++ src/Components/Pages/Home/Cars/Cars.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Car from "../../Car/Car";
+import { bounceInUp } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 
 const Cars = () => {
   const [cars, setCars] = useState([]);
@@ -9,14 +11,27 @@ const Cars = () => {
       .then((data) => setCars(data));
   }, []);
 
+  // Animation
+  const styles = {
+    bounce: {
+      animation: "x 1s",
+      animationName: Radium.keyframes(bounceInUp, "bounceInUp"),
+    },
+  };
+
   return (
     <div className="my-10">
       <h2 className="font-bold text-4xl">Our Latest Cars</h2>
-      <div className="cars-container grid gap-4 grid-cols-3 mx-4 my-6">
-        {cars.slice(0, 6).map((car) => (
-          <Car key={car.id} car={car}></Car>
-        ))}
-      </div>
+      <StyleRoot>
+        <div
+          style={styles.bounceInUp}
+          className="cars-container grid gap-4 grid-cols-3 mx-4 my-6"
+        >
+          {cars.slice(0, 6).map((car) => (
+            <Car key={car.id} car={car}></Car>
+          ))}
+        </div>
+      </StyleRoot>
     </div>
   );
 };
